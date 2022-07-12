c65ac3c05e6b0ed9049a8a8055297c081e27271a
xiaoyao <765485868@qq.com>
2022-07-12 23:26:33 +0800

    update code

--- shell/cmake.sh
+++ shell/cmake.sh
@@ -6,7 +6,7 @@ CMAKE_ROOT=/root/program/cmake-${CMAKE_VERSION}
 mkdir -p ${CMAKE_ROOT}
 cd ${CMAKE_ROOT}
 
-wget -q https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION-Linux-x86_64.sh
+wget https://ghproxy.com/https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION-Linux-x86_64.sh
 chmod +x ./cmake-$CMAKE_VERSION-Linux-x86_64.sh
 ./cmake-$CMAKE_VERSION-Linux-x86_64.sh --skip-license > /dev/null
 update-alternatives --install /usr/bin/cmake cmake $(pwd)/bin/cmake 1000 --force
